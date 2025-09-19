3ca8fb0793e91b40da75bd296cfd5231424c619c
nabhya8013 <nabhyamaheshwari8013@gmail.com>
2025-09-19 15:03:13 +0530

    new things , not ready for final @khanakshah27

--- scripts/augment_data.py
+++ scripts/augment_data.py
@@ -14,3 +14,16 @@ def add_spikes(intensity, num_spikes=5, spike_height=0.5):
     for i in indices:
         corrupted[i] += spike_height * np.random.uniform(0.5, 1.0)
     return corrupted
+
+def augment_spectrum(intensity, apply_noise=True, apply_baseline=True, apply_spikes=True):
+    """Apply augmentation pipeline to one spectrum."""
+    augmented = intensity.copy()
+    
+    if apply_noise:
+        augmented = add_noise(augmented)
+    if apply_baseline:
+        augmented = add_baseline(augmented)
+    if apply_spikes:
+        augmented = add_spikes(augmented)
+    
+    return augmented
